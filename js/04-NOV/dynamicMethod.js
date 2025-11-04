9960333119ae03d87951b87d8be3661c905de44b
someone2508 <143279417+someone2508@users.noreply.github.com>
2025-11-04 21:49:41 +0530

    4th nov

---
+++ js/04-NOV/dynamicMethod.js
@@ -0,0 +1,9 @@
+const parentList = document.getElementById('list');
+
+let newLi = undefined;
+
+// parentList.appendChild(newLi);
+
+// parentList.replaceChild(newList, existingChild);
+
+// parentList.removeChild(newLi);
---
+++ js/04-NOV/mightyTexts.js
@@ -0,0 +1,7 @@
+let el = document.getElementById("demo");
+
+console.log(el.innerText);      // Hello World (visible/readable text only)
+
+console.log(el.textContent);    // /n   Hello World/n   
+
+console.log(el.innerHTML);      // /n   <b>Hello</b> World /n
